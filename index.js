a4b22fe7cb229fdfaff956bf046d7f3d318690e2
Alberto Fernández <alberto.fernandez.491@gmail.com>
2017-01-26 11:51:47 +0000

    removed es6 let declaration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var flyd = require('flyd')
 module.exports = flyd.curryN(2, function (streams, source$) {
   if (streams.constructor !== Array) streams = [streams];
   return flyd.combine(function (source, self) {
-    let res = [source()]
+    var res = [source()]
     for (var i = 0; i < streams.length; ++i) {
       if (!streams[i].hasVal) return;
       res.push(streams[i]())
